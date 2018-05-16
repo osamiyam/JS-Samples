c639a13a72a40e9e86286f75a16e5d8c27e632b3
Osami Yamamoto <osami@meijo-u.ac.jp>
2018-05-16 23:09:10 +0900

    added Algorithm07

---
+++ Algorithm07/bits2.js
@@ -0,0 +1,12 @@
+var puts = console.log
+
+function print_bits(n, prefix){
+    if (n == 0) puts(prefix)
+    else{
+        print_bits(n - 1, prefix + '0')
+        print_bits(n - 1, prefix + '1')
+    }
+}
+
+print_bits(10, "")
+
