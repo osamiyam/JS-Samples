a230471988ce8b8a9c72ac0b61c56db1103a1d94
Osami Yamamoto <osami@meijo-u.ac.jp>
2018-05-18 12:45:43 +0900

    added three programs

---
+++ Algorithm07/hanoi.js
@@ -0,0 +1,12 @@
+function hanoi(n, x, y, z){
+    if (n == 0) return
+    else {
+        hanoi(n - 1, x, z, y);
+        puts("ディスク " + n + " を " + x + " から " +
+             z + " へ移動" )
+        hanoi(n - 1, y, x, z);
+    }
+}
+
+hanoi(6, 'X', 'Y', 'Z')
+
---
+++ Algorithm07/qsort.js
@@ -0,0 +1,40 @@
+ans = []
+function qsort(lst){
+    if (lst.length <= 1) ans = ans.concat(lst)
+    else{
+        var key = lst[0]
+        var low = []
+        var eq = [key]
+        var high = []
+        for (var i = 1; i < lst.length; i++){
+            var m = lst[i]
+            if (key > m) low.push(m)
+            else if (key < m) high.push(m)
+            else eq.push(m)
+        }
+        qsort(low)
+        ans = ans.concat(eq)
+        qsort(high)
+    }
+}
+
+function test1(){
+    qsort([3, 5, 4, 8, 2, 3, 1])
+    puts(ans)
+}
+
+function test2(){
+    var ans = []
+    prob = []
+    for (var i = 0; i < 10000; i++){
+        prob.push(Math.random())
+    }
+    t1 = new Date()
+    qsort(prob)
+    t2 = new Date()
+    puts(ans[0] + " " + ans[5000] + " " + ans[9999])
+    puts((t2 - t1) + " ms")
+}
+
+test1()
+// test2()
